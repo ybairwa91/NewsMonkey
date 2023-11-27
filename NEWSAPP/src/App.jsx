4fd1f56a3e34a0613c887d9b8f33903046ac5160
Yogesh Kumar Bairwa <ybairwa786@gmail.com>
2023-11-27 16:14:50 +0530

    NewsApp

---
+++ NEWSAPP/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  c = "jogn";
+  render() {
+    return (
+      <>
+        <h1>Hello this is my first class based component</h1>
+      </>
+    );
+  }
+}
+
+export default App;
